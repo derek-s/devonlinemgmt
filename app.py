abb94a64749b718b3fab4d1989f66bea594815b2
Derek.S <derekseli@outlook.com>
2017-11-24 16:38:29 +0800

    add func annotation

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ SECRET_KEY = '12efc6ca97aefb8e1f6a589c6f334a405bca977bc9cec023f193ee975379e153'
 app = Flask(__name__)
 
 app.secret_key = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:123a+-@192.168.10.105/devonlie"
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = "mysql://root:123a+-@192.168.10.105/devonlie"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
@@ -22,15 +23,19 @@ db.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
+
 @app.route("/", methods=['GET', 'POST'])
 @login_required
 def hello():
     return "hello world Flask"
 
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    """登录函数，处理登录页面，使用login.html模板"""
     if request.method == 'POST':
-        user = User.query.filter_by(username=request.form['username'], password=request.form['password']).first()
+        user = User.query.filter_by(
+            username=request.form['username'], password=request.form['password']).first()
         if user:
             login_user(user)
             flash("logged in!")
@@ -40,9 +45,12 @@ def login():
     form = LoginForm()
     return render_template('login.html', form=form)
 
+
 @login_manager.user_loader
 def load_user(id):
+    """登录user_loader回调"""
     return User.query.filter_by(id=int(id)).first()
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.100', port=5000, debug=True)
