560942f90779965a33dff349118947800d88b482
Derek.S <derekseli@outlook.com>
2017-11-24 18:02:22 +0800

    login page stylesheet

--- forms.py
+++ forms.py
@@ -2,10 +2,15 @@
 # -*- coding:UTF-8 -*-
 
 from flask_wtf import FlaskForm
-from wtforms import RadioField, SubmitField, StringField, PasswordField
+from wtforms import SubmitField, StringField, PasswordField
 from wtforms.validators import DataRequired, Length
 
+
 class LoginForm(FlaskForm):
-    username  = StringField(u'用户名', validators=[DataRequired(), Length(1, 24)])
-    password = PasswordField(u'密码', validators=[DataRequired(), Length(1, 24)])
-    submit = SubmitField(u'登录')
+    username  = StringField(
+        '', validators=[
+            DataRequired(message= u'用户名不能为空'), Length(1, 24)], render_kw={"placeholder": u"用户名","required": False})
+    password = PasswordField(
+        '', validators=[
+            DataRequired(message= u'密码不能为空'), Length(1, 24)], render_kw={"placeholder": u"密码"})
+    submit = SubmitField(u'登录', render_kw={"style": "width:100%"})
