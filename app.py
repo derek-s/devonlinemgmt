88e3c853dba8a35331d1b15798caedb7d079befb
Derek.S <derekseli@outlook.com>
2017-11-26 17:16:30 +0800

    test test func

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_bootstrap import Bootstrap
 
 from forms import LoginForm
 from ext import db, login_manager
-from models import User
+from models import *
 
 SECRET_KEY = '12efc6ca97aefb8e1f6a589c6f334a405bca977bc9cec023f193ee975379e153'
 
@@ -22,13 +22,16 @@ bootstrap = Bootstrap(app)
 db.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = "login"
+ulevelinfo = ''
 
 
 @app.route("/", methods=['GET', 'POST'])
 @login_required
 def hello():
     """测试函数"""
-    return "hello world Flask"
+    devstatus = Dev_DeviceStatus.query.all()
+    print User.is_admin()
+    return render_template('index.html', dev_status = devstatus)
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -40,6 +43,7 @@ def login():
         if user:
             login_user(user)
             flash("logged in!")
+            ulevelinfo = str(user.userlevel)
             return redirect(url_for('hello'))
         else:
             flash("logged filed")
@@ -47,10 +51,19 @@ def login():
     return render_template('login.html', form=form)
 
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('you have logout!')
+    return redirect(url_for('login'))
+
+
 @login_manager.user_loader
 def load_user(id):
     """登录user_loader回调"""
     return User.query.filter_by(id=int(id)).first()
+
 
 
 if __name__ == '__main__':
