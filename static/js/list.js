41e5cc5876ef1dee08a05a9dc40b2228bfc9a3b3
Derek.S <derekseli@outlook.com>
2017-11-29 23:38:32 +0800

    add query func

--- static/js/list.js
+++ static/js/list.js
@@ -2,9 +2,23 @@ $(document).ready(
     function() {
         $.getJSON($SCRIPT_ROOT + 'list', {}, function(data) {
             $.each(data, function(one) {
-                $("table#devinfolist>tbody").append("<tr></tr>")
+                $("table#devinfolist>tbody").append('<tr class="jsondata"></tr>')
                 eachone = data[one]
-                $("table#devinfolist>tbody tr:last-child").append("<td>" + eachone.id + "</td><td>" + eachone.Location + "</td><td>" + eachone.HostName + "</td><td>" + eachone.LAA + "</td><td>" + eachone.HigherlinkIP + "</td><td>" + eachone.HigherlinkPort + "</td><td>" + eachone.DeviceModel + "</td>")
+                $("table#devinfolist>tbody tr:last-child").append("<td>" + eachone.ID + "</td><td>" + eachone.Location + "</td><td>" + eachone.HostName + "</td><td>" + eachone.LAA + "</td><td>" + eachone.HigherlinkIP + "</td><td>" + eachone.HigherlinkPort + "</td><td>" + eachone.DeviceModel + "</td>")
             })
+        })
+        $("button#serach").bind('click', function() {
+            var strkey = encodeURI($('input[name="devinfoserp"]').val(), "utf-8")
+            $.getJSON($SCRIPT_ROOT + '_querydevinfo', {
+                keyword: strkey
+            }, function(data) {
+                $("tr.jsondata").empty()
+                $.each(data, function(one) {
+                    $("table#devinfolist>tbody").append('<tr class="jsondata"></tr>')
+                    eachone = data[one]
+                    $("table#devinfolist>tbody tr:last-child").append("<td>" + eachone.ID + "</td><td>" + eachone.Location + "</td><td>" + eachone.HostName + "</td><td>" + eachone.LAA + "</td><td>" + eachone.HigherlinkIP + "</td><td>" + eachone.HigherlinkPort + "</td><td>" + eachone.DeviceModel + "</td>")
+                })
+            });
+            return false;
         })
     })
