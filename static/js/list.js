a69b8259130b2cc8e244e71cb195f5cba33f1d46
Derek.S <derekseli@outlook.com>
2017-12-01 22:27:14 +0800

    change stylesheet templates

--- static/js/list.js
+++ static/js/list.js
@@ -1,30 +1,52 @@
 $(document).ready(
     function() {
-        var str2;
-        $.getJSON($SCRIPT_ROOT + 'list', {}, function(data) {
-            $.each(data, function(one) {
-                $("table#devinfolist>tbody").append('<tr class="jsondata"></tr>')
-                eachone = data[one]
-                $("table#devinfolist>tbody tr:last-child").append("<td>" + eachone.ID + "</td><td class='location'>" + eachone.Location + "</td><td>" + eachone.HostName + "</td><td>" + eachone.LAA + "</td><td>" + eachone.HigherlinkIP + "</td><td>" + eachone.HigherlinkPort + "</td><td class='model'>" + eachone.DeviceModel + "</td>")
+        $("a#ajaxnext").bind('click', function() {
+            $(this).text("正在加载，请稍后……")
+            pagenum = $("li.active>a").text()
+            datacount = $("tr.jsondata").length
+            textabc = $.getJSON($SCRIPT_ROOT + 'list', {
+                count: datacount,
+                pagenum: pagenum
+            }, function(data) {
+
+                trc = ""
+                var tra = '<tr class="jsondata">'
+                var trb = '</tr>'
+                $.each(data, function(one) {
+                    eachone = data[one]
+                    trc += tra + "<td>" + eachone.ID + "</td><td class='location'>" + eachone.Location + "</td><td>" + eachone.HostName + "</td><td>" + eachone.LAA + "</td><td>" + eachone.HigherlinkIP + "</td><td>" + eachone.HigherlinkPort + "</td><td class='model'>" + eachone.DeviceModel + "</td>" + trb
+                    hasnext = eachone.next
+                })
+                $("table#devinfolist>tbody tr:last-child").after(trc)
+                if (!hasnext) {
+                    $("a#ajaxnext").remove()
+                }
+            }).fail(function(status) {
+                if (status.status == '404') {
+                    $("a#ajaxnext").remove()
+                }
             })
         })
-        $("button#serach").bind('click', function() {
+        $("button#indexsbutton").bind('click', function() {
             strkey = encodeURI($('input[name="devinfoserp"]').val(), "utf-8")
             $.getJSON($SCRIPT_ROOT + '_querydevinfo', {
                 keyword: strkey
             }, function(data) {
                 $("tr.jsondata").empty()
+                trc = ""
+                var tra = '<tr class="jsondata">'
+                var trb = '</tr>'
                 $.each(data, function(one) {
-                    $("table#devinfolist>tbody").append('<tr class="jsondata"></tr>')
                     eachone = data[one]
-                    $("table#devinfolist>tbody tr:last-child").append("<td>" + eachone.ID + "</td><td>" + eachone.Location + "</td><td>" + eachone.HostName + "</td><td>" + eachone.LAA + "</td><td>" + eachone.HigherlinkIP + "</td><td>" + eachone.HigherlinkPort + "</td><td class='model'>" + eachone.DeviceModel + "</td>")
+                    trc += tra + "<td>" + eachone.ID + '</td><td class="location">' + eachone.Location + "</td><td>" + eachone.HostName + "</td><td>" + eachone.LAA + "</td><td>" + eachone.HigherlinkIP + "</td><td>" + eachone.HigherlinkPort + '</td><td class="model">' + eachone.DeviceModel + "</td>" + trb
                 })
+                $("table#devinfolist>tbody tr:last-child").after(trc)
             });
             return false;
         })
         $("table#devinfolist>tbody").on('click', '.location', function() {
             var str2;
-            var tablehead = "<table class='tabledevinfo table table-striped table-hover'><tbody><tr><th>序号</th><th>楼宇名称</th><th>楼栋号码</th><th>楼层</th><th>房间号</th><th>机柜数量</th></tr>"
+            var tablehead = '<table class="tabledevinfo table table-striped table-hover"><tbody><tr><th>序号</th><th>楼宇名称</th><th>楼栋号码</th><th>楼层</th><th>房间号</th><th>机柜数量</th></tr>'
             var tableend = "</tbody></table>"
             strkey1 = encodeURI($(this).html(), "utf-8")
             $.getJSON($SCRIPT_ROOT + '_querylvr', {
@@ -46,7 +68,7 @@ $(document).ready(
         })
         $("table#devinfolist>tbody").on('click', '.model', function() {
             var str2;
-            var tablehead = "<table class='tabledevinfo table table-striped table-hover'><tbody><tr><th>序号</th><th>设备名称</th><th>设备分类</th><th>设备序列号</th><th>使用情况</th><th>唯一ID</th></tr>"
+            var tablehead = '<table class="tabledevinfo table table-striped table-hover"><tbody><tr><th>序号</th><th>设备名称</th><th>设备分类</th><th>设备序列号</th><th>使用情况</th><th>唯一ID</th></tr>'
             var tableend = "</tbody></table>"
             strkey1 = encodeURI($(this).html(), "utf-8")
             $.getJSON($SCRIPT_ROOT + '_querydev', {
@@ -60,12 +82,13 @@ $(document).ready(
                 layer.open({
                     title: '设备信息',
                     type: 1,
-                    skin: 'layui-layer-rim', //加上边框
-                    area: ['600px', '240px'], //宽高
+                    skin: 'layui-layer-rim',
+                    area: ['600px', '240px'],
                     content: tablehead + str2 + tableend
                 });
             })
 
 
         })
-    })
+    }
+)
