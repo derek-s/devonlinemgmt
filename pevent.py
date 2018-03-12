57df55a86a9734c82e5fd759d4e77b1fbfc4a6b9
Derek.S <derekseli@outlook.com>
2018-03-12 14:49:31 +0800

    add person events code

---
+++ pevent.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/12 11:01
+# @Author  : Derek.S
+# @Site    : 
+# @File    : pevent.py
+
+from ext import db
+from models import DevPEvents
+
+
+def peventsindexlist(username):
+    """
+    返回前台个人待办事项列表
+    :return:
+    """
+    event = DevPEvents.query.order_by(DevPEvents.id.desc()).paginate(1, 10)
+    event_items = event.items
+    return event_items
+
