5ca4b99b096ac23fed444ddaa2d65eb8a26cf0ce
Derek.S <derekseli@outlook.com>
2018-03-16 21:28:21 +0800

    增加后台数据查询部分代码

--- views/admin.py
+++ views/admin.py
@@ -82,6 +82,42 @@ def query_list():
     )
 
 
+@adminbg.route("/admin/query/serach")
+@login_required
+@admin_required
+def query_serach():
+    """
+    数据查询搜索页
+    :return:
+    """
+    request.script_root = url_for('indexview.index', _external=True)
+    page = request.args.get('page', 1, type=int)
+    word = request.args.get('keyword', "", type=str)
+    serach = unquote(b64decode(word)).decode('utf-8')
+    serp = Dev_DeviceStatus.query.filter(
+        (Dev_DeviceStatus.Campus.like("%" + serach + "%"), "")[serach is None] |
+        (Dev_DeviceStatus.Location.like("%" + serach + "%"), "")[serach is None] |
+        (Dev_DeviceStatus.RoomNo.like("%" + serach + "%"), "")[serach is None] |
+        (Dev_DeviceStatus.HostName.like("%" + serach + "%"), "")[serach is None] |
+        (Dev_DeviceStatus.LAA.like("%" + serach + "%"), "")[serach is None] |
+        (Dev_DeviceStatus.HigherlinkIP.like("%" + serach + "%"), "")[serach is None] |
+        (Dev_DeviceStatus.HigherlinkPort.like("%" + serach + "%"), "")[serach is None] |
+        (Dev_DeviceStatus.DeviceModel.like("%" + serach + "%"), "")[serach is None]
+    ).order_by(Dev_DeviceStatus.Campus.desc())
+    paginateion = serp.paginate(
+        page, per_page=Setting().pagination
+    )
+    count = serp.count()
+    posts = paginateion.items
+    eventlog(
+        "[搜索]" + serach.encode('utf-8') + " 第" + str(page) + "页"
+    )
+    return render_template(
+        '/admin/dbquery_serach.html', posts=posts, count=count, pagination=paginateion, keyword=serach
+    )
+
+
+
 @adminbg.route("/admin/lvrmanage")
 @login_required
 @admin_required
