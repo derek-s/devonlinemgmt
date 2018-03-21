5125a2b230bea16f89371437380da0984b3088ff
Derek.S <derekseli@outlook.com>
2018-03-21 14:23:40 +0800

    分拆代码-2

--- views/admin.py
+++ views/admin.py
@@ -15,7 +15,10 @@ from pevent import peventsindexlist
 import json
 
 from .admin_dbquery import admin_query, admin_query_list, admin_query_serach
-from .admin_lvr import  lvr_manager_index
+from .admin_lvr import  lvr_manager_index, lvr_manager_ajaxquery
+from .admin_lvr import lvr_list_get, lvr_list_post, lvr_search_get, lvr_search_post
+
+
 adminbg = Blueprint('adminbg', __name__)
 
 
@@ -100,11 +103,12 @@ def lvrmanager():
     :return: 返回数据查询结果并构建相应页面
     """
     result = lvr_manager_index()
-    return render_template('/admin/lvrmanager.html',
-                           posts=result['posts'],
-                           count=result['count'],
-                           pagination=result['pagination'],
-                           campus=result['campus'])
+    return render_template(
+        '/admin/lvrmanager.html',
+        posts=result['posts'],
+        count=result['count'],
+        pagination=result['pagination'],
+        campus=result['campus'])
 
 
 @adminbg.route("/admin/lvrmanage/_queryipage", methods=['POST'])
@@ -115,24 +119,7 @@ def querylvrpage():
     后台弱电间信息ajax查询
     :return:
     """
-    count = request.values.get('count', None, type=int)
-    pagenum = request.values.get('pagenum', None, type=int)
-    page_num = ((count/Setting().pagination+pagenum), 0)[count/Setting().pagination == 0]
-    lvrinfo = Dev_LVRInfo.query.order_by(Dev_LVRInfo.Campus.desc()).paginate(
-        (page_num), per_page=Setting().pagination
-    )
-    lvrinfotemp = []
-    hasnext = {
-        'next': lvrinfo.has_next
-    }
-    for lvrx in lvrinfo.items:
-        jsonlist = lvrx.to_json()
-        jsonlist.update(hasnext)
-        lvrinfotemp.append(jsonlist)
-    eventlog(
-        "弱电间ajax加载下一页" + " 第" + str(page_num) + "页"
-    )
-    return jsonify(lvrinfotemp)
+    return lvr_manager_ajaxquery()
 
 
 @adminbg.route("/admin/lvrmanage/list", methods=['GET', 'POST'])
@@ -144,63 +131,21 @@ def querylvrlist():
     :return:
     """
     if request.method == 'GET':
-        page = request.args.get('page', 1, type=int)
-        request.script_root = url_for('indexview.index', _external=True)
-        campusname = b64decode(unquote(request.args.get('campusname', "", type=str)))
-        buildname = b64decode(unquote(request.args.get('buildname', "", type=str)))
-        lvrinfo = lvrlistsql(campusname, buildname)
-        paginateion = lvrinfo.paginate(
-            page, per_page=Setting().pagination
-        )
-        count = lvrinfo.count()
-        posts = paginateion.items
-        campus = Dev_Campus.query.all()
-        eventlog(
-            "[后台弱电间信息 查询校区/楼宇]" + campusname + buildname + " 第" + str(page) + "页"
-        )
+        result = lvr_list_get()
         return render_template(
-            "/admin/lvrmanage_list.html", posts=posts, count=count, pagination=paginateion, campus=campus,
-            ctitle=campusname.decode('utf-8'), btitle=buildname.decode('utf-8')
+            "/admin/lvrmanage_list.html",
+            posts=result['posts'],
+            count=result['count'],
+            pagination=result['pagination'],
+            campus=result['campus'],
+            ctitle=result['ctitle'],
+            btitle=result['btitle']
         )
     elif request.method == 'POST':
-        pagenum = request.values.get('pagenum', None, type=int)
-        count = request.values.get('count', None, type=int)
-        campusname = unquote(request.values.get('campusname', "", type=str))
-        buildname = unquote(request.values.get('buildname', "", type=str))
-        lvrinfo = lvrlistsql(campusname, buildname)
-        page_num = ((count / Setting().pagination + pagenum), 0)[count / Setting().pagination == 0]
-        paginateion = lvrinfo.paginate(
-            page_num, per_page=Setting().pagination
-        )
-        lvrinfolist = []
-        hasnext = {
-            'next': paginateion.has_next
-        }
-
-        for lvrline in paginateion.items:
-            jsonlist = lvrline.to_json()
-            jsonlist.update(hasnext)
-            lvrinfolist.append(jsonlist)
-        eventlog(
-            "[ajax加载弱电间查询页面下一页]" + campusname + buildname + " 第" + str(page_num) + "页"
-        )
-        return jsonify(lvrinfolist)
+        return lvr_list_post()
     else:
         abort(404)
 
-
-def lvrlistsql(campusname, buildname):
-    """
-    校区查询SQL
-    :param campusname: 校区名称
-    :param buildname: 楼宇名称
-    :return:
-    """
-    lvrinfo = Dev_LVRInfo.query.filter(
-        (Dev_LVRInfo.Campus.like("%" + campusname + "%"), "")[campusname is None],
-        (Dev_LVRInfo.BuildName.like("%" + buildname + "%"), "")[buildname is None]
-    ).order_by(Dev_LVRInfo.Campus.desc())
-    return lvrinfo
 
 
 @adminbg.route("/admin/lvrmanage/search", methods=['GET', 'POST'])
@@ -212,61 +157,18 @@ def querylvrserach():
     :return:
     """
     if request.method == 'GET':
-        request.script_root = url_for('indexview.index', _external=True)
-        page = request.args.get('page', 1, type=int)
-        word = request.args.get('keyword', "", type=str)
-        serach = unquote(b64decode(word)).decode('utf-8')
-        serp = lvrsearchsql(serach)
-        paginateion = serp.paginate(
-            page, per_page=Setting().pagination
-        )
-        count = serp.count()
-        posts = paginateion.items
-        eventlog(
-            "[管理后台弱电间 搜索]" + serach.encode('utf-8') + " 第" + str(page) + "页"
-        )
+        result = lvr_search_get()
         return render_template(
-            "/admin/lvrmanage_serach.html", posts=posts, count=count, pagination=paginateion, keyword=serach
+            "/admin/lvrmanage_serach.html",
+            posts=result['posts'],
+            count=result['count'],
+            pagination=result['pagination'],
+            keyword=result['keyword']
         )
     elif request.method == 'POST':
-        pagenum = request.values.get('pagenum', None, type=int)
-        count = request.values.get('count', None, type=int)
-        word = request.values.get('keyword', None, type=str)
-        search = b64decode(unquote(word)).decode('utf-8')
-        serp = lvrsearchsql(search)
-        page_num = ((count / Setting().pagination + pagenum), 0)[count / Setting().pagination == 0]
-        paginateion = serp.paginate(
-            page_num, per_page=Setting().pagination
-        )
-        serachresult = []
-        hasnext = {
-            'next': paginateion.has_next
-        }
-        for serachone in paginateion.items:
-            jsonlist = serachone.to_json()
-            jsonlist.update(hasnext)
-            serachresult.append(jsonlist)
-        eventlog(
-            "[ajax加载搜索页面下一页]" + search.encode('utf-8') + " 第" + str(page_num) + "页"
-        )
-        return jsonify(serachresult)
+        return lvr_search_post()
     else:
         abort(500)
-
-
-def lvrsearchsql(search):
-    """
-    弱电间搜索SQL
-    :return:
-    """
-    serp = Dev_LVRInfo.query.filter(
-        (Dev_LVRInfo.Campus.like("%" + search + "%"), "")[search is None] |
-        (Dev_LVRInfo.BuildName.like("%" + search + "%"), "")[search is None] |
-        (Dev_LVRInfo.BuildNo.like("%" + search + "%"), "")[search is None] |
-        (Dev_LVRInfo.RoomNo.like("%" + search + "%"), "")[search is None] |
-        (Dev_LVRInfo.LVRNo.like("%" + search + "%"), "")[search is None]
-    ).order_by(Dev_LVRInfo.Campus.desc())
-    return serp
 
 
 @adminbg.route("/admin/dvrmanage", methods=['GET', 'POST'])
--- views/admin_lvr.py
+++ views/admin_lvr.py
@@ -36,3 +36,152 @@ def lvr_manager_index():
         'campus': campus
     }
     return result
+
+
+
+def lvr_manager_ajaxquery():
+    count = request.values.get('count', None, type=int)
+    pagenum = request.values.get('pagenum', None, type=int)
+    page_num = ((count / Setting().pagination + pagenum), 0)[count / Setting().pagination == 0]
+    lvrinfo = Dev_LVRInfo.query.order_by(Dev_LVRInfo.Campus.desc()).paginate(
+        (page_num), per_page=Setting().pagination
+    )
+    lvrinfotemp = []
+    hasnext = {
+        'next': lvrinfo.has_next
+    }
+    for lvrx in lvrinfo.items:
+        jsonlist = lvrx.to_json()
+        jsonlist.update(hasnext)
+        lvrinfotemp.append(jsonlist)
+    eventlog(
+        "弱电间ajax加载下一页" + " 第" + str(page_num) + "页"
+    )
+    return jsonify(lvrinfotemp)
+
+
+def lvr_list_get():
+    page = request.args.get('page', 1, type=int)
+    request.script_root = url_for('indexview.index', _external=True)
+    campusname = b64decode(unquote(request.args.get('campusname', "", type=str)))
+    buildname = b64decode(unquote(request.args.get('buildname', "", type=str)))
+    lvrinfo = lvrlistsql(campusname, buildname)
+    paginateion = lvrinfo.paginate(
+        page, per_page=Setting().pagination
+    )
+    count = lvrinfo.count()
+    posts = paginateion.items
+    campus = Dev_Campus.query.all()
+    eventlog(
+        "[后台弱电间信息 查询校区/楼宇]" + campusname + buildname + " 第" + str(page) + "页"
+    )
+    result = {
+        'posts': posts,
+        'count': count,
+        'pagination': paginateion,
+        'campus': campus,
+        'ctitle': campusname.decode('utf-8'),
+        'btitle': buildname.decode('utf-8')
+    }
+    return result
+
+
+def lvr_list_post():
+    pagenum = request.values.get('pagenum', None, type=int)
+    count = request.values.get('count', None, type=int)
+    campusname = unquote(request.values.get('campusname', "", type=str))
+    buildname = unquote(request.values.get('buildname', "", type=str))
+    lvrinfo = lvrlistsql(campusname, buildname)
+    page_num = ((count / Setting().pagination + pagenum), 0)[count / Setting().pagination == 0]
+    paginateion = lvrinfo.paginate(
+        page_num, per_page=Setting().pagination
+    )
+    lvrinfolist = []
+    hasnext = {
+        'next': paginateion.has_next
+    }
+
+    for lvrline in paginateion.items:
+        jsonlist = lvrline.to_json()
+        jsonlist.update(hasnext)
+        lvrinfolist.append(jsonlist)
+    eventlog(
+        "[ajax加载弱电间查询页面下一页]" + campusname + buildname + " 第" + str(page_num) + "页"
+    )
+    return jsonify(lvrinfolist)
+
+
+def lvrlistsql(campusname, buildname):
+    """
+    校区查询SQL
+    :param campusname: 校区名称
+    :param buildname: 楼宇名称
+    :return:
+    """
+    lvrinfo = Dev_LVRInfo.query.filter(
+        (Dev_LVRInfo.Campus.like("%" + campusname + "%"), "")[campusname is None],
+        (Dev_LVRInfo.BuildName.like("%" + buildname + "%"), "")[buildname is None]
+    ).order_by(Dev_LVRInfo.Campus.desc())
+    return lvrinfo
+
+
+def lvr_search_get():
+    request.script_root = url_for('indexview.index', _external=True)
+    page = request.args.get('page', 1, type=int)
+    word = request.args.get('keyword', "", type=str)
+    serach = unquote(b64decode(word)).decode('utf-8')
+    serp = lvrsearchsql(serach)
+    pagination = serp.paginate(
+        page, per_page=Setting().pagination
+    )
+    count = serp.count()
+    posts = pagination.items
+    eventlog(
+        "[管理后台弱电间 搜索]" + serach.encode('utf-8') + " 第" + str(page) + "页"
+    )
+    result = {
+        'posts': posts,
+        'count': count,
+        'pagination': pagination,
+        'keyword': serach
+    }
+    return result
+
+
+def lvr_search_post():
+    pagenum = request.values.get('pagenum', None, type=int)
+    count = request.values.get('count', None, type=int)
+    word = request.values.get('keyword', None, type=str)
+    search = b64decode(unquote(word)).decode('utf-8')
+    serp = lvrsearchsql(search)
+    page_num = ((count / Setting().pagination + pagenum), 0)[count / Setting().pagination == 0]
+    paginateion = serp.paginate(
+        page_num, per_page=Setting().pagination
+    )
+    serachresult = []
+    hasnext = {
+        'next': paginateion.has_next
+    }
+    for serachone in paginateion.items:
+        jsonlist = serachone.to_json()
+        jsonlist.update(hasnext)
+        serachresult.append(jsonlist)
+    eventlog(
+        "[ajax加载搜索页面下一页]" + search.encode('utf-8') + " 第" + str(page_num) + "页"
+    )
+    return jsonify(serachresult)
+
+
+def lvrsearchsql(search):
+    """
+    弱电间搜索SQL
+    :return:
+    """
+    serp = Dev_LVRInfo.query.filter(
+        (Dev_LVRInfo.Campus.like("%" + search + "%"), "")[search is None] |
+        (Dev_LVRInfo.BuildName.like("%" + search + "%"), "")[search is None] |
+        (Dev_LVRInfo.BuildNo.like("%" + search + "%"), "")[search is None] |
+        (Dev_LVRInfo.RoomNo.like("%" + search + "%"), "")[search is None] |
+        (Dev_LVRInfo.LVRNo.like("%" + search + "%"), "")[search is None]
+    ).order_by(Dev_LVRInfo.Campus.desc())
+    return serp
