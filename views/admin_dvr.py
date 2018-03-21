38383944c376380a86d2e5ae3afb04e20507a3b2
Derek.S <derekseli@outlook.com>
2018-03-21 15:46:39 +0800

    包引用调整

--- views/admin_dvr.py
+++ views/admin_dvr.py
@@ -5,16 +5,11 @@
 # @Site    : 
 # @File    : admin_dvr.py
 
-from flask import Blueprint, jsonify, render_template, request, url_for, flash, session, redirect, abort
-from flask_login import login_required, current_user
-from decorators import admin_required
-from models import Dev_Loging, Setting, Dev_Note, Dev_DeviceStatus, Dev_Campus, Dev_LVRInfo, Dev_DeviceInfo, DevDevType
+from flask import jsonify, request, url_for
+from models import Setting, Dev_DeviceInfo, DevDevType
 from log import eventlog
 from base64 import b64decode
 from urllib import unquote
-from sysmanger import optionsupdate
-import arrow
-from ext import db
 
 
 def dvr_manage_get():
