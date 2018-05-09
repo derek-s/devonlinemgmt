15417c7ef4070dca09344487b22c0a06cd377d4c
Derek.S <derekseli@outlook.com>
2018-05-09 21:32:14 +0800

    添加清空日志表路由

--- views/admin.py
+++ views/admin.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, request, url_for, flash, session, 
 from flask_login import login_required, current_user
 from decorators import admin_required
 from models import Dev_Loging, Setting
-from log import eventlog
+from log import eventlog, emptylog
 from base64 import b64decode
 from urllib import unquote
 from sysmanger import optionsupdate
@@ -514,28 +514,35 @@ def logviews():
     操作日志界面
     :return: 返回数据查询结果并构建相应页面
     """
-    page = request.values.get('page', 1, type=int)
-    uname = request.values.get('username', "", type=str)
-    username = b64decode(unquote(uname))
-    catsname = request.values.get('cats', "", type=str)
-    cats = b64decode(unquote(catsname))
-    date = request.values.get('date', "", type=str)
-    logdata = Dev_Loging.query.filter(
-        (Dev_Loging.UserName.like("%" + username + "%"), "")[username is None],
-        (Dev_Loging.Date.like("%" + date + "%"), "")[date is None],
-        (Dev_Loging.Log.like("%" + cats + "%"), "")[cats is None]
-    )
-    paginateion = logdata.paginate(
-        page, per_page=Setting().pagination
-    )
-    posts = paginateion.items
-    count = logdata.count()
-    eventlog("[查看日志]" + " 第" + str(page) +"页")
-    return render_template(
-        '/admin/log.html', posts=posts, count=count, pagination=paginateion,
-        page=page, username=uname, cats=catsname, date=date,
-        thuname=username.decode('utf-8'), thcats=cats.decode('utf-8')
-    )
+    if request.method == "GET":
+        page = request.values.get('page', 1, type=int)
+        uname = request.values.get('username', "", type=str)
+        username = b64decode(unquote(uname))
+        catsname = request.values.get('cats', "", type=str)
+        cats = b64decode(unquote(catsname))
+        date = request.values.get('date', "", type=str)
+        logdata = Dev_Loging.query.filter(
+            (Dev_Loging.UserName.like("%" + username + "%"), "")[username is None],
+            (Dev_Loging.Date.like("%" + date + "%"), "")[date is None],
+            (Dev_Loging.Log.like("%" + cats + "%"), "")[cats is None]
+        )
+        paginateion = logdata.paginate(
+            page, per_page=Setting().pagination
+        )
+        posts = paginateion.items
+        count = logdata.count()
+        eventlog("[查看日志]" + " 第" + str(page) +"页")
+        return render_template(
+            '/admin/log.html', posts=posts, count=count, pagination=paginateion,
+            page=page, username=uname, cats=catsname, date=date,
+            thuname=username.decode('utf-8'), thcats=cats.decode('utf-8')
+        )
+    elif request.method == "POST":
+        cleancode = request.values.get('logcode', 'None', type=str)
+        if cleancode == "all":
+            return emptylog()
+        else:
+            abort(500)
 
 
 @adminbg.route("/admin/notecreate", methods=['GET', 'POST'])
