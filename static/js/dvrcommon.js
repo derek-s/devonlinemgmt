4d2d1ddfb5c04033c33ee9e92e824b1567c74fab
Derek.S <derekseli@outlook.com>
2018-05-15 21:58:53 +0800

    修改js

--- static/js/dvrcommon.js
+++ static/js/dvrcommon.js
@@ -62,9 +62,18 @@ $(document).ready(
             var keyword = $("input#indexserach").val()
             window.open(Flask.url_for('adminbg.dvrsearch') + "?keyword=" + encodeURIComponent($.base64.encode(keyword)))
         })
-        $('.dvrsearch').click(function() {
+        //页面跳转
+        $('button.dvrsearch').click(function() {
             var page = $('#transfer_page').val();
-            window.location.href = Flask.url_for('adminbg.dvrsearch',{keyword: keyword_url}) + "&page=" + page;
+            window.location.href = Flask.url_for('adminbg.dvrsearch', {keyword: keyword_url}) + "&page=" + page;
+        })
+        $('button.dvrlist').click(function() {
+            var page = $('#transfer_page').val();
+            window.location.href = Flask.url_for('adminbg.dvrmanagelist', {devtype: devtype_info, devonline: devonline_info}) + "&page=" + page;
+        })
+        $('button.dvrmanage').click(function () {
+            var page = $('#transfer_page').val();
+            window.location.href = Flask.url_for('adminbg.dvrmanage') + "?page=" + page;
         })
         $("a.dvrsajaxnet").bind('click', function () {
             //list加载下一页
@@ -97,6 +106,71 @@ $(document).ready(
             }).fail(function (status) {
                 if (status.status == '404') {
                     $("a.dvrsajaxnet").remove()
+                }
+            })
+        })
+
+        $("a.dvrlistajaxnet").bind('click', function () {
+            console.log(1)
+            $(this).text("正在加载，请稍后……")
+            var pagenum = $("li#pnactive>a").text()
+            var datacount = $("tr.jsondata").length
+            var devtype = encodeURIComponent($.base64.encode($('#dropdown_devtype').text()), 'utf-8')
+            var devonline = encodeURIComponent($.base64.encode($('#dropdown_devonline').text()), 'utf-8')
+            var posturl = Flask.url_for('adminbg.dvrmanagelist', {devtype: devtype_info, devonline: devonline_info})
+            $.post(posturl, {
+                count: datacount,
+                pagenum: pagenum,
+                devtype: devtype,
+                devonline: devonline
+            }, function (data) {
+                var trc = ""
+                var tra = '<tr class="jsondata">'
+                var trb = '</tr>'
+                $.each(data, function (one) {
+                    eachone = data[one]
+                    trc += tra + "<td>" + eachone.ID + "</td><td><input id='oper-" + eachone.ID + "' type='checkbox' name='oper' value='"+ eachone.ID + "' /></td><td>" + eachone.DeviceName + "</td><td>" + eachone.DeviceCategory + "</td><td>" + eachone.DeviceSN + "</td><td>" + eachone.DeviceCondition + "</td><td>" + eachone.DeviceID + "</td><td>" + "<button onclick='' type'button' class='btn btn-link inline_button_link'>管理</button>" + "</td>" + trb
+                    hasnext = eachone.next
+                })
+                $("table#devinfolist>tbody tr:last-child").after(trc)
+                if (!hasnext) {
+                    $("a.dvrlistajaxnet").remove()
+                } else {
+                    $("a.dvrlistajaxnet").text('下一页')
+                }
+            }).fail(function (status) {
+                if (status.status == '404') {
+                    $("a.dvrlistajaxnet").remove()
+                }
+            })
+        })
+        $("a.dvrmanageajax").bind('click', function () {
+            //list加载下一页
+            $(this).text("正在加载，请稍后……")
+            var pagenum = $("li#pnactive>a").text()
+            var datacount = $("tr.jsondata").length
+            var posturl = Flask.url_for('adminbg.dvrmanage')
+            $.post(posturl, {
+                count: datacount,
+                pagenum: pagenum
+            }, function (data) {
+                var trc = ""
+                var tra = '<tr class="jsondata">'
+                var trb = '</tr>'
+                $.each(data, function (one) {
+                    eachone = data[one]
+                    trc += tra + "<td>" + eachone.ID + "</td><td><input id='oper-" + eachone.ID + "' type='checkbox' name='oper' value='"+ eachone.ID + "' /></td><td>" + eachone.DeviceName + "</td><td>" + eachone.DeviceCategory + "</td><td>" + eachone.DeviceSN + "</td><td>" + eachone.DeviceCondition + "</td><td>" + eachone.DeviceID + "</td><td>" + "<button onclick='' type'button' class='btn btn-link inline_button_link'>管理</button>" + "</td>" + trb
+                    hasnext = eachone.next
+                })
+                $("table#devinfolist>tbody tr:last-child").after(trc)
+                if (!hasnext) {
+                    $("a.dvrmanageajax").remove()
+                } else {
+                    $("a.dvrmanageajax").text('下一页')
+                }
+            }).fail(function (status) {
+                if (status.status == '404') {
+                    $("a.dvrmanageajax").remove()
                 }
             })
         })
